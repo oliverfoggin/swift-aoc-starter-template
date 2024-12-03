9d35e28fa7e4727c708388d55f2dedb00539ff63
Oliver Foggin <oliverfoggin@me.com>
2024-12-03 10:02:20 +0000

    Start day 3

---
+++ Sources/Day03.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct Day03: AdventDay, Sendable {
+  // Save your data in a corresponding text file in the `Data` directory.
+  let data: String
+  let day = 0
+  let puzzleName: String = "--- Day 0: Placeholder! ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() async throws -> Int {
+    0
+  }
+}
+
+// Add any extra code and types in here to separate it from the required behaviour
+extension Day03 {}
